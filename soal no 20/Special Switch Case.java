5bae75c3d5f5db94e5fc56685dbbb34dc016f09d
irzhan-armani <76740252+irzhan-armani@users.noreply.github.com>
2020-12-31 12:43:41 -0800

    reupload

--- soal no 20/Special Switch Case.java
+++ soal no 20/Special Switch Case.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Main {
+class SpecialSwitch {
   public static void main(String[] args) {
      Scanner input = new Scanner(System.in);
         System.out.print("");
