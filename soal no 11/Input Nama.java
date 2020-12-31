128b09584784f442951d827b5db6e06002c700f2
irzhan-armani <76740252+irzhan-armani@users.noreply.github.com>
2020-12-30 22:09:56 -0800

    reupload

--- soal no 11/Input Nama.java
+++ soal no 11/Input Nama.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
+import java.util.Scclass Main{public static void main(String[]args){
 
-public class Main {
-    public static void main(String[] args) {
+Scanner input=new Scanner(System.in);String nama=input.nextLine();System.out.println("Hai "+nama);
 
-        Scanner input = new Scanner(System.in);
-        String nama = input.nextLine();
-        System.out.println("Hai " + nama);
-
-    }
-}
+}}
